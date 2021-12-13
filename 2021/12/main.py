a6f13b6b12ca85271ff2778cf543bed9e79b46db
Jonas E. Gilje <1jonasgilje@gmail.com>
2021-12-13 15:36:16 +0100

    12 og 13 ferdig

--- 2021/12/main.py
+++ 2021/12/main.py
@@ -1,4 +1,4 @@
-INPUT_FILE = "/home/jonasgilje/Documents/Advent_of_Code/2020/01/input"
+INPUT_FILE = "/home/jonasgilje/Documents/Advent_of_Code/2021/12/input"
 
 
 def get_input():
@@ -6,25 +6,91 @@ def get_input():
 
     with open(INPUT_FILE, "r") as file:
         for line in file:
-            input_list.append(line)
+            input_list.append(line.strip())
     
     return input_list
 
 
 def sanitize_input(input_list):
-    pass
+    return [l.split("-") for l in input_list]
 
 
 def part1(input_list):
+    input_list = sanitize_input(input_list)
+    edge_dict = {}
 
-    answer = None
+    for a, b in input_list:
+        if a in edge_dict:
+            edge_dict[a].append(b)
+        else:
+            edge_dict[a] = [b]
+        if b in edge_dict:
+            edge_dict[b].append(a)
+        else:
+            edge_dict[b] = [a]
+    
+    complete_paths = []
+    current_paths = [["start"]]
+
+    while current_paths:
+        path = current_paths.pop()
+        edges = edge_dict[path[-1]]
+        for edge in edges:
+            if "a" <= edge[0] <= "z" and edge in path:
+                continue
+            new_path = [*path, edge]
+            if new_path[0] == "start" and new_path[-1] == "end":
+                complete_paths.append(new_path)
+            else:
+                current_paths.append(new_path)
+
+    answer = len(complete_paths)
     return answer
 
 
 def part2(input_list):
+    input_list = sanitize_input(input_list)
+    edge_dict = {}
 
-    answer = None
+    for a, b in input_list:
+        if a in edge_dict:
+            edge_dict[a].append(b)
+        else:
+            edge_dict[a] = [b]
+        if b in edge_dict:
+            edge_dict[b].append(a)
+        else:
+            edge_dict[b] = [a]
+    
+    small_caves = edge_dict.keys()
+    small_caves = [c for c in small_caves if "a" <= c[0] <= "z" and c not in ("start", "end")]
+    complete_paths = []
+
+    for okay_visit_twice in small_caves:
+        
+        current_paths = [["start"]]
+        
+        while current_paths:
+            path = current_paths.pop()
+            edges = edge_dict[path[-1]]
+            for edge in edges:
+                if edge == okay_visit_twice:
+                    if len([edge for e in path if e == edge]) > 1:
+                        continue
+                else:
+                    if "a" <= edge[0] <= "z" and edge in path:
+                        continue
+                
+                new_path = [*path, edge]
+                if new_path[0] == "start" and new_path[-1] == "end":
+                    if new_path not in complete_paths:
+                        complete_paths.append(new_path)
+                else:
+                    current_paths.append(new_path)
+
+    answer = len(complete_paths)
     return answer
+
 
 
 def main():
@@ -32,6 +98,7 @@ def main():
     ans1 = part1(input_list)
     ans2 = part2(input_list)
 
+    #ans1=5958, ans2=150426
     print(f"{ans1=}, {ans2=}")
 
 
--- 2021/13/main.py
+++ 2021/13/main.py
@@ -1,4 +1,6 @@
-INPUT_FILE = "/home/jonasgilje/Documents/Advent_of_Code/2020/01/input"
+import itertools
+
+INPUT_FILE = "/home/jonasgilje/Documents/Advent_of_Code/2021/13/input"
 
 
 def get_input():
@@ -6,24 +8,55 @@ def get_input():
 
     with open(INPUT_FILE, "r") as file:
         for line in file:
-            input_list.append(line)
+            input_list.append(line.strip())
     
     return input_list
 
 
 def sanitize_input(input_list):
-    pass
+    idx = input_list.index("")
+    dots = [tuple(map(int, t)) for t in [l.split(",") for l in input_list[:idx]]]
+    folds = [(a[-1], int(b)) for a, b in [l.split("=") for l in input_list[idx+1:]]]
+    return dots, folds
 
 
 def part1(input_list):
+    dots, folds = sanitize_input(input_list)
+    max_x, max_y = max(d[0] for d in dots), max(d[1] for d in dots)
 
-    answer = None
+    fold = folds[0]
+    dots = [((a, b) if a < fold[1] else (max_x-a, b)) for a, b in dots]
+    #dots = [((a, b) if b < fold[1] else (a, max_y-b)) for a, b in dots]
+
+    max_x, max_y = max(d[0] for d in dots), max(d[1] for d in dots)
+
+    answer = 0
+    dot_set = set(dots)
+    for x, y in itertools.product(range(max_x+1), range(max_y+1)):
+        if (x, y) in dot_set:
+            answer += 1
+
     return answer
 
 
 def part2(input_list):
+    dots, folds = sanitize_input(input_list)
 
+    for fold in folds:
+        
+        if fold[0] == "x":
+            dots = [((a, b) if a < fold[1] else (fold[1]*2-a, b)) for a, b in dots]
+        else:
+            dots = [((a, b) if b < fold[1] else (a, fold[1]*2-b)) for a, b in dots]
+    
+    max_x, max_y = max(d[0] for d in dots), max(d[1] for d in dots)
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            print("#" if (x, y) in dots else ".", end="")
+        print()
+    
     answer = None
+
     return answer
 
 
